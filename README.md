b29a51ebfa4386db58b32f42472d0c66d3583200
taixingbi <bitaihang@gmail.com>
2021-10-29 17:00:29 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## java-logging-spring-webflux
+
+### reference
+```
+https://piotrminkowski.wordpress.com/2019/10/15/reactive-logging-with-spring-webflux-and-logstash/```
---
+++ src/main/java/hello/App.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args)  {
+        logger.debug("----------app start----------");
+
+        logger.debug("----------app end----------");
+    }
+}
